fd29fb5964fa28119a1e5203b198edca3f2d9877
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-12-23 12:48:34 +1000

    (generic) moving towards using memory arenas

--- include/generic/map.h
+++ include/generic/map.h
@@ -6,22 +6,29 @@
 /* NOTE: key is typically limited to 32 chars long */
 /* NOTE: value is typically limited to 1024 chars long */
 typedef struct {
-    char* key;
-    char* value;
+    unsigned int  key;  /* index into MapFile.strings */
+    unsigned int  value;
 } KeyValue;
 
 
 typedef struct {
-    unsigned int  num_brushes;
     unsigned int  num_keyvalues;
-    Brush        *brushes;
-    KeyValue     *keyvalues;
+    unsigned int  first_keyvalue;
+    unsigned int  num_brushes;
+    unsigned int  first_brush;
 } Entity;
 
 
 typedef struct {
     unsigned int  num_entities;
+    unsigned int  num_brushes;
+    /* brushsides? */
+    unsigned int  num_keyvalues;
+    unsigned int  strings_len;
     Entity       *entities;
+    Brush        *brushes;
+    KeyValue     *keyvalues;
+    char         *strings;
 } MapFile;
 
 
